9c53c31537207c042edeb3b8a9edb3860edb2f66
soubhagya <soubhagyasahoo328@gmail.com>
2024-03-28 18:54:41 +0530

    main class modified

--- src/main/java/in/sahoo/it/Application.java
+++ src/main/java/in/sahoo/it/Application.java
@@ -2,12 +2,16 @@ package in.sahoo.it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+ 	ConfigurableApplicationContext context=	SpringApplication.run(Application.class, args);
+ 	int i=10;
+ 	System.out.println(i);
+ 	context.close();
 	}
 
 }
